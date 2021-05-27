41a7e55213a76c8dc73dd0b90da5b19f81a468a7
Rubenrh <ruben.romeroherrero@gmail.com>
2021-05-27 09:05:25 +0200

    Actualizacion detalles

--- repositories/commentRepository.js
+++ repositories/commentRepository.js
@@ -2,18 +2,25 @@ const Post = require("../models/Post");
 const Comment = require("../models/Comment");
 const User = require("../models/User");
 
+// ibamos a requerir ese include en allposts y en postbyid, así optimizamos codigo
+const populate = {
+  include: [
+    { model: User, attributes: ["name"] },
+    // indicamos el title del post en el que se encuentra el comment
+    {
+      model: Post,
+      attributes: ["title"],
+      include: { model: User, attributes: ["name"] },
+    },
+  ],
+};
+
 exports.findAllComments = async () => {
-  return await Comment.findAll({
-    include: [
-      { model: User, attributes: ["name"] },
-      // indicamos el title del post en el que se encuentra el comment
-      { model: Post, attributes: ["title"] },
-    ],
-  });
+  return await Comment.findAll(populate);
 };
 
 exports.findCommentById = async (id) => {
-  return await Comment.findByPk(id);
+  return await Comment.findByPk(id, populate);
 };
 
 exports.insertComment = async (commentDetails) => {
